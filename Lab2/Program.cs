33fe8e5a71f46908e2610647033555d18200fc84
Dylan Pogue <POGUEDJ@ETSU.EDU>
2022-09-02 23:03:16 -0400

    q2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -12,3 +12,17 @@ Console.WriteLine("Enter the second number: ");
 num2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+
+// #2 Multiply table
+int bigNumber;
+
+Console.Write("Enter the number you like to use ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the number you would like to use for the multiplication table:");
+bigNumber = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < bigNumber; i++)
+{
+    Console.WriteLine($"{num1} * {i} = {num1 * i}");
+}
